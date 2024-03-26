a192efb9b8c7af6d3426b0fe47ddb9f9f78a66d0
ajorgep <alejorpas2@alu.edu.gva.es>
2024-03-26 20:23:36 +0100

    Commit

--- app/src/main/java/es/ieslavereda/calculator/MainActivity.java
+++ app/src/main/java/es/ieslavereda/calculator/MainActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView textView;
     private Button buttonClear;
@@ -16,8 +18,11 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonLess;
     private Button buttonMultiply;
     private Button buttonSplit;
+    private Button buttonPoint;
+    private CheckBox checkBoxSettings;
     private Operacion operacion;
     private Double operando;
+    private RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
         buttonLess = findViewById(R.id.buttonLess);
         buttonMultiply = findViewById(R.id.buttonMultiply);
         buttonSplit = findViewById(R.id.buttonSplit);
+        buttonPoint = findViewById(R.id.buttonPoint);
+        checkBoxSettings = findViewById(R.id.checkBoxSettings);
+        radioGroup = findViewById(R.id.radioGroup);
 
         buttonClear.setOnClickListener(v -> textView.setText("0"));
 
@@ -58,6 +66,10 @@ public class MainActivity extends AppCompatActivity {
             textView.setText("0");
         });
 
+        buttonPoint.setOnClickListener(v -> {
+            textView.setText(textView.getText() + ".");
+        });
+
         buttonEqual.setOnClickListener(v -> {
             if (operacion == Operacion.SUMA) {
                 textView.setText(String.valueOf(operando + Double.parseDouble(textView.getText().toString())));
@@ -69,8 +81,55 @@ public class MainActivity extends AppCompatActivity {
                 textView.setText(String.valueOf(operando / Double.parseDouble(textView.getText().toString())));
             }
         });
+
+
+        checkBoxSettings.setOnClickListener(v -> {
+            if (checkBoxSettings.isChecked()) {
+                radioGroup.setVisibility(View.VISIBLE);
+                checkBoxSettings.setText("Deshabilitar opciones");
+
+            } else {
+                checkBoxSettings.setText("Habilitar opciones");
+                radioGroup.setVisibility(View.GONE);
+
+                radioGroup.clearCheck();
+
+                buttonSum.setVisibility(View.VISIBLE);
+                buttonLess.setVisibility(View.VISIBLE);
+                buttonMultiply.setVisibility(View.VISIBLE);
+                buttonSplit.setVisibility(View.VISIBLE);
+            }
+        });
+
+        radioGroup.setOnCheckedChangeListener((radioGroup, i) -> {
+            if (i == R.id.radioButtonSum) {
+                buttonSum.setVisibility(View.GONE);
+                buttonLess.setVisibility(View.VISIBLE);
+                buttonMultiply.setVisibility(View.VISIBLE);
+                buttonSplit.setVisibility(View.VISIBLE);
+
+            } else if (i == R.id.radioButtonLess) {
+                buttonLess.setVisibility(View.GONE);
+                buttonSum.setVisibility(View.VISIBLE);
+                buttonMultiply.setVisibility(View.VISIBLE);
+                buttonSplit.setVisibility(View.VISIBLE);
+
+            } else if (i == R.id.radioButtonMultiply) {
+                buttonMultiply.setVisibility(View.GONE);
+                buttonSum.setVisibility(View.VISIBLE);
+                buttonLess.setVisibility(View.VISIBLE);
+                buttonSplit.setVisibility(View.VISIBLE);
+
+            } else if (i == R.id.radioButtonSplit) {
+                buttonSplit.setVisibility(View.GONE);
+                buttonSum.setVisibility(View.VISIBLE);
+                buttonLess.setVisibility(View.VISIBLE);
+                buttonMultiply.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
+    @Override
     public void onClick(View view) {
 
         if (textView.getText().equals("0")) {
